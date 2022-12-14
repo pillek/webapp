59eb81bf444082dd8049eb0dbc9abc5e8a5aadea
Fpraeger <florian.praeger@t-online.de>
2022-12-14 12:44:12 +0100

    Änderung der Dateien

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
+from views import views
 
 app = Flask(__name__)
+app.register_blueprint(views, url_prefix="/views")
 
-@app.route('/tasks')
-def list():
-    return 
-
-
-
+   
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)
+
+

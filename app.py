7eae19c908624d2c79a648d226722f4091daaea8
pillek <pillek48@gmail.com>
2022-12-15 16:06:13 +0100

    New website and POST and GET

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from views import views
 
 app = Flask(__name__)
 
 app.register_blueprint(views, url_prefix="/views")
+
+app = Flask(name)
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+@app.route('/', methods=["GET", 'POST'])
+def inputTask():
+   task = request.form["nm"]
+   return render_template('index.html'), 
+
+
+
+# @app.route('/', methods=["GET", 'POST'])
+# def showTask():
+#       task = request.args.get('nm')
+#       return render_template('index.html')
 
 
 if __name__ == '__main__':
